e20f4ac2e30c6026e85b851e2c39e1eaf9f786fb
ItsNotRohit <rohitghughtyal2@gmail.com>
2022-11-02 12:38:06 +0530

    this is first commit
    Signed-off-by: ItsNotRohit <rohitghughtyal2@gmail.com>

--- src/App.js
+++ src/App.js
@@ -1,23 +1,31 @@
 import logo from './logo.svg';
 import './App.css';
 
+function Header(){
+  return (
+
+    <header className="App-header" > ReactFacts </header>
+    
+  )
+}
+
+function MainContent(){
+  return(
+    <ul>
+      <li> hi </li>
+      <li> there </li>
+      <li> hmm </li>
+      <li> colll </li>
+    </ul>
+  )
+}
+
+
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Header />
+      <MainContent />
     </div>
   );
 }
